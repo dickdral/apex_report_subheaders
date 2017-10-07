1ffc5719c8d89bf671782e9ad68d4963291bb56e
dickdral <dick.dral@detora.nl>
2017-10-07 09:40:12 +0200

    Some tweaks

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Oracle APEX Dynamic Action Plugin -  Report Subheaders
 Oracle Apex plug-in for generating subheaders in Classical Reports. 
 Based on a normal report you can indicate one column to function as subheader. For each occasion where the value of this column changes an extra row is created over the full width of the report, containing the new value of the column. The subheader column in the report is hidden. 
+Default the table headings are repeated after each subheader, and this can be disabled. 
 
 ## Change history
 - V1.0    Initial version
@@ -10,9 +11,11 @@ The plugin can be used with Apex 5.0 and Apex 5.1.
 
 ## Install
 - Import plugin file "dynamic_action_plugin_nl_detora_apex_report_subheaders.sql" from the source directory into your application
+- Add a column to act as subheader
 - Create an After Refresh dynamic action for the Region with the report for which you want to generate subheaders
 - add an action for the plugin
 - fill in the name of the column to be used for the subheaders
+- you can disable repeating subheaders if neccessary
 
 ## Plugin Settings
 No settings are needed.
--- server/js/apex_report_subheaders.js
+++ server/js/apex_report_subheaders.js
@@ -5,12 +5,39 @@
 // global namespace
 var apex_report_subheaders = {
 
-generate_subheaders: function ( report_selector, subheader_column )
-{
-  
-  console.log('execute report subheaders');
-  
-},
+  generate_subheaders: function ( report_selector, subheader_column, repeat_headings )
+  {
+    // console.log('execute report subheaders');
+    var subheader;
+    var selector = '[headers="'+subheader_column+'"]';
+    var headings = $(report_selector).find('th').closest('tr');
+    console.log(headings);
+    
+    $(report_selector).find(selector).each( function() 
+    {
+        var new_subheader = $(this).html();
+        if ( new_subheader != subheader )            
+        { 
+            // add subheader
+            subheader = new_subheader;
+            tr = $(this).closest('tr');
+            tr.before('<tr><td colspan="100" class="t-Report-cell subheader">'+subheader+'</td></tr>');
+            
+            // repeat headings if requested
+            if ( repeat_headings == "Y" ) 
+                { tr.before($(headings).clone()); }
+        }
+    });
+      
+    // hide original heading if repeat headings are requested
+    if ( repeat_headings == "Y" ) 
+       { $(headings).hide(); }
+      
+    // generate styles to hide subheader column and format the subheaders
+    $('head').append('<style>#'+subheader_column+',[headers="'+subheader_column+'"]{display:none;}</style>');
+    $('head').append('<style>td.t-Report-cell.subheader{background-color: lightgrey!important;font-weight: bold;}</style>');
+
+  },
 
     // function that gets called from plugin
     doIt: function() {
@@ -19,6 +46,7 @@ generate_subheaders: function ( report_selector, subheader_column )
         var vElementsArray  = daThis.affectedElements;
         var report_selector = '#'+$(vElementsArray[0]).attr('id');
         var subheader_column = daThis.action.attribute01;
-        apex_report_subheaders.generate_subheaders(report_selector,subheader_column);
+        var repeat_headings  = daThis.action.attribute02;
+        apex_report_subheaders.generate_subheaders(report_selector,subheader_column,repeat_headings);
     }
 };
